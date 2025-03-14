b3711ed1904fe3e039898ac8361ecf8b07117506
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-14 16:58:34 +0100

    add status 404 in the show function

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,6 +14,13 @@ function show(req, res) {
     const postSlug = req.params.slug
     const post = blog.find(post => post.slug === postSlug)
     console.log(post);
+
+    if(!post){
+        return res.status(404).json({
+            error: '404',
+            message: 'Post not found'
+        })
+    }
     res.json(post)
 }
 
