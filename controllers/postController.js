e6d02e7934da06bd4030a17ba9b81f838bbcb90f
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-14 16:10:02 +0100

    add milestone 1

---
+++ controllers/postController.js
@@ -0,0 +1,32 @@
+function index(req, res) {
+    res.send(`Return all posts`)
+}
+
+function show(req, res) {
+    res.send(`Show the post with slug: ${req.params.slug}`)
+}
+
+function store(req, res) {
+    res.send(`Store new post`)
+}
+
+function update(req, res) {
+    res.send(`Update the post with slug: ${req.params.slug}`)
+}
+
+function modify(req, res) {
+    res.send(`Modify the post whith slug: ${req.params.slug}`)
+}
+
+function destroy(req, res) {
+    res.send(`Delete the post with slug: ${req.params.slug}`)
+}
+
+module.exports = {
+    index, 
+    show,
+    store, 
+    update,
+    modify,
+    destroy
+}
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const postsRouter = require('./routers/posts')
+
 /* listening */
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
@@ -12,3 +14,5 @@ app.get('/', (req, res) => {
     res.send(`Welcome on our server`)
 })
 
+/* posts route */
+app.use('/api/v1/posts', postsRouter)
