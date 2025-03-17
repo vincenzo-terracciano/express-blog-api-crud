45483f11821f08506db396aafeb17975b9ecba27
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-17 13:43:27 +0100

    fix store function and add update function

--- controllers/postController.js
+++ controllers/postController.js
@@ -26,10 +26,12 @@ function show(req, res) {
 
 function store(req, res) {
     console.log(req.body);
+
+    const newSlag = req.body.title.toLowerCase().split(" ").join("-");
     
     const newPost = {
         title: req.body.title,
-        slug: req.body.slug,
+        slug: newSlag,
         content: req.body.content,
         image: req.body.image,
         tags: req.body.tags
@@ -43,7 +45,26 @@ function store(req, res) {
 }
 
 function update(req, res) {
-   
+    const postSlug = req.params.slug;
+    const post = blog.find(post => post.slug === postSlug)
+    console.log(post);
+    
+    if(!post){
+        return res.status(404).json({
+                error: '404',
+                message: 'Post not found'
+            })
+    }
+    
+    post.title = req.body.title;
+    post.slug = req.body.slug;
+    post.content = req.body.content;
+    post.image = req.body.image;
+    post.tags = req.body.tags;
+
+    console.log(blog);
+    
+    res.json(post)
 }
 
 function modify(req, res) {
