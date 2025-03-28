c988ff3b9eb9d4245efdd7b7e2d985dd067b908e
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-28 16:25:02 +0100

    add CORS support and new image assets

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 3000;
 
 const postsRouter = require('./routers/posts')
-
 const serverError = require('./middlewares/serverError')
-
 const error_404 = require('./middlewares/error_404')
 
+app.use(express.static('public'))
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5182'
+}))
 
 /* listening */
 app.listen(port, () => {
